997cf958ca296914ff060e48d3f3c1fa5673a24f
lin onetwo <linonetwo012@gmail.com>
2022-04-05 00:13:22 +0800

    Update run-demo.mjs

--- scripts/run-demo.mjs
+++ scripts/run-demo.mjs
@@ -7,4 +7,4 @@ console.log(`nodejs: http://localhost:3001/
 html: http://localhost:3000/`);
 // And a nodejs server to receive data
 cd('..');
-await $`tiddlywiki . --listen port=3001 root-tiddler=$:/core/save/lazy-images`;
+await $`tiddlywiki +plugins/tiddlywiki/filesystem +plugins/tiddlywiki/tiddlyweb . --listen port=3001 root-tiddler=$:/core/save/lazy-images`;
