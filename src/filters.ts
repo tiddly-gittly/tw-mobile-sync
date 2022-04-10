f70e92c910c88eff0cd4422454af2ed28cf8fe4e
lin onetwo <linonetwo012@gmail.com>
2022-04-10 17:27:21 +0800

    feat: including system tids like plugin change

--- src/filters.ts
+++ src/filters.ts
@@ -1,5 +1,5 @@
 export function getDiffFilter(lastSync: string | undefined) {
-  return `[all[]!is[system]] :filter[get[modified]compare:date:gt[${lastSync ?? ''}]]`;
+  return `[all[]] :filter[get[modified]compare:date:gt[${lastSync ?? ''}]]`;
 }
 
 /**
