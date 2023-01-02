6de93c86c65d0cece980205a2e542a847de600f6
Lin Onetwo <linonetwo012@gmail.com>
2023-01-02 22:10:03 +0800

    feat: show caption in dialog
    fixes #8

--- src/browser-background-sync.ts
+++ src/browser-background-sync.ts
@@ -215,14 +215,14 @@ class BackgroundSyncManager {
           // TODO: handle conflict
           $tw.wiki.addTiddler(tiddler);
         });
-        const changedTitleDisplayLimit = 3;
+        const changedTitleDisplayLimit = 5;
         const clientText = take(changedTiddlersFromClient, changedTitleDisplayLimit)
-          .map((tiddler) => tiddler.title as string)
+          .map((tiddler) => tiddler.caption ?? (tiddler.title as string))
           .join(' ');
         const clientCount =
           changedTiddlersFromClient.length > changedTitleDisplayLimit ? `And ${changedTiddlersFromClient.length - changedTitleDisplayLimit} more` : '';
         const serverText = take(changedTiddlersFromServer, changedTitleDisplayLimit)
-          .map((tiddler) => tiddler.title as string)
+          .map((tiddler) => tiddler.caption ?? (tiddler.title as string))
           .join(' ');
         const serverCount =
           changedTiddlersFromServer.length > changedTitleDisplayLimit ? `And ${changedTiddlersFromServer.length - changedTitleDisplayLimit} more` : '';
