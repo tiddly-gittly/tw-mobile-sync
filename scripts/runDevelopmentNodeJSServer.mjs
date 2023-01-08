c28cff7c9225cae491427ece7500a4988032e265
linonetwo <linonetwo012@gmail.com>
2023-01-08 23:31:41 +0800

    feat: root-tiddler=$:/core/save/lazy-images

--- scripts/runDevelopmentNodeJSServer.mjs
+++ scripts/runDevelopmentNodeJSServer.mjs
@@ -8,4 +8,4 @@ import { getPort } from 'get-port-please';
 
 const port = await getPort({ port: 3001 });
 echo(`Open http://${await internalIpV4()}:${port}/`);
-await $`tiddlywiki ./dist --listen port=${port} host=0.0.0.0 sse-enabled=yes`;
+await $`tiddlywiki ./dist --listen port=${port} host=0.0.0.0 root-tiddler=$:/core/save/lazy-images`;
