cc88fb32d2c40c485de620185f0c82f7af6d1fcb
lin onetwo <linonetwo012@gmail.com>
2025-03-25 15:15:26 +0800

    fix: disable 3 way merge for now

--- src/tw-mobile-sync/data/mergeTiddler.ts
+++ src/tw-mobile-sync/data/mergeTiddler.ts
@@ -15,29 +15,33 @@ export function mergeTiddler(a: ITiddlerFields, b: ITiddlerFields): ITiddlerFiel
     throw new Error(`Cannot merge tiddlers with different titles: ${a.title} and ${b.title}`);
   }
   const newerOne = a.modified > b.modified ? a : b;
-  if (leftIsBinaryRightIsString(a, b)) {
+  const aIsBinary = isBinaryTiddler(a);
+  const bIsBinary = isBinaryTiddler(b);
+  if ((!aIsBinary && !bIsBinary) || (bIsBinary && bIsBinary)) {
+    return newerOne;
+  } else if (leftIsBinaryRightIsString(aIsBinary, bIsBinary)) {
     // we choose binary, because we assume binary data is more important than pure text
     return a;
-  } else if (leftIsBinaryRightIsString(b, a)) {
+  } else if (leftIsBinaryRightIsString(bIsBinary, aIsBinary)) {
     return b;
-  } else if (isBinaryTiddler(a) && isBinaryTiddler(b)) {
-    return newerOne;
   }
+  // TODO: currently only return a;
+  return a;
   // both is string tiddler, we can merge them using diff-match-patch algorithm
   // FIXME: Currently not working, it needs `c` that is an older version from server to work (3-way-merge), otherwise it will just use `b.text` as the merged text
-  // FIXME: `Error when processing tiddler { title: '2024-03-19T13:45:00+08:00', created: '20240319063429986', creator: '林一二', startDate: '20240319054500000', endDate: '20240319063000000', calendarEntry: 'yes', tags: 'xxx', modified: '20240319063429986', modifier: '林一二', caption: 'yyy', type: 'text/vnd.tiddlywiki', text: '' } Error: Unknown call format to make() at make ($:/plugins/linonetwo/tw-mobile-sync/server-sync-v1-endpoint.js:1:19900) at mergeTiddler ($:/plugins/linonetwo/tw-mobile-sync/server-sync-v1-endpoint.js:1:25987) at $:/plugins/linonetwo/tw-mobile-sync/server-sync-v1-endpoint.js:9:1558 at Array.forEach (<anonymous>) at Object.handler2 [as handler] ($:/plugins/linonetwo/tw-mobile-sync/server-sync-v1-endpoint.js:9:1426) at IncomingMessage.<anonymous> ($:/core/modules/server/server.js:308:10) at IncomingMessage.emit (node:events:517:28) at endReadableNT (node:internal/streams/readable:1368:12) at processTicksAndRejections (node:internal/process/task_queues:82:21)`
-  const patches = makePatches(a.text, b.text);
-  const [mergedText] = applyPatches(patches, a.text);
-  const fields: ITiddlerFields = {
-    ...newerOne,
-    text: mergedText,
-  };
-  return fields;
+  // const dmpObject = new dmp.diff_match_patch()
+  // const patches = dmpObject.diff_main(a.text, b.text);
+  // const [mergedText] = applyPatches(patches, a.text);
+  // const fields: ITiddlerFields = {
+  //   ...newerOne,
+  //   text: mergedText,
+  // };
+  // return fields;
 }
 
-function leftIsBinaryRightIsString(a: ITiddlerFields, b: ITiddlerFields) {
+function leftIsBinaryRightIsString(aIsBinary: boolean, bIsBinary: boolean) {
   // a is binary, b is string
-  if (isBinaryTiddler(a) && !isBinaryTiddler(b)) {
+  if (aIsBinary && !bIsBinary) {
     return true;
   }
   return false;
